e8be59b510f053a5b5cdb628bdb929f26173cf0c
arunsagarrai01 <arunsagarrai6@gmail.com>
2026-09-03 14:32:50 +0545

    Learn about column and text, style text

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -6,7 +6,24 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 20,
+          children: [
+            Text("Login here", style: TextStyle(
+              fontSize: 28,
+                color: Colors.deepOrangeAccent,
+                fontWeight: FontWeight.bold
+            ),),
 
+            Text("Welcome back you have been missed!",
+                textAlign: .center,
+                style: TextStyle(fontSize: 20,
+                fontWeight: FontWeight.bold),),
+          ],
+        ),
+      ),
     );
   }
 }
